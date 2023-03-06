c7fbd3350b88539009755732bf9b08f3bfb82041
olar88 <parkeunyeon18@gmail.com>
2023-03-06 13:44:45 +0800

    加router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Products from './component/Products'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/product" element={<Products />} />
+        </Routes>
+      </Router>
       <Products />
     </>
   )
