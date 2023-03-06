f3ba17dcf8db2ca1b081e4f1a909ddcadc90b874
olar88 <parkeunyeon18@gmail.com>
2023-03-06 14:30:42 +0800

    navbar元件

--- src/memberLogin/LoginPage.js
+++ src/memberLogin/LoginPage.js
@@ -2,11 +2,16 @@ import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import './../styles/login.css'
+import Navbar from '../component/Navbar'
 
 // import AuthContext from '../contexts/AuthContext'
 
 function LoginPage() {
-  return <></>
+  return (
+    <>
+      <Navbar />
+    </>
+  )
 }
 
 export default LoginPage
